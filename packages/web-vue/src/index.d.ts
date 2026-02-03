c3cb4de8dd3895293181400a8a0e4ca15d68ff13
lizixiong <2254295494@qq.com>
2026-02-03 16:10:30 +0800

    delete: 删除弃用文件

--- packages/web-vue/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { FormRenderProps } from "@meta-engine/adapter-vue";
-declare const FormRender: import("vue").DefineComponent<
-  FormRenderProps,
-  {
-    /* setup 返回类型 */
-  }
->;
-
-export default FormRender;
